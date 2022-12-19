90c8f20deb80bb468dbc0dc5eb54faf9341e789d
rin1208 <sora1208sky@gmail.com>
2022-12-19 10:27:18 +0900

    コメントアウト

--- src/component/Children2.tsx
+++ src/component/Children2.tsx
@@ -5,7 +5,7 @@ import RecoilCount from "./recoilCounter";
 const Children2: React.FC = () => {
   const [num, setNum] = useRecoilState(children2Atom);
 
-  const num2 = useRecoilValue(children1Atom);
+  // const num2 = useRecoilValue(children1Atom);
 
   const getColor = () => Math.floor(Math.random() * 255);
 
@@ -16,7 +16,7 @@ const Children2: React.FC = () => {
     <div>
       <h2 style={style}>{num}</h2>
       <RecoilCount count={num} setCount={setNum} />
-      <h2 style={style}>children1Atomの値 : {num2} </h2>
+      {/* <h2 style={style}>children1Atomの値 : {num2} </h2> */}
     </div>
   );
 };
